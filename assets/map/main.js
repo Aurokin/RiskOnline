ad29dd6b5bb224357397381a4e96b312551214e6
Summer <throwawaysummer@gmail.com>
2014-11-06 21:55:23 -0600

    added script tag

--- assets/map/main.js
+++ assets/map/main.js
@@ -3,6 +3,9 @@ We have to use an asset manager (located in assetManager.js) to download every l
 otherwise the map would be imperfect (some images may not appear or load slowly)
  */
 
+<!-- adding: Summer --!>
+<script src="jquery-1.11.1.min.js"></script>
+
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload('images/map_grey.jpg');
