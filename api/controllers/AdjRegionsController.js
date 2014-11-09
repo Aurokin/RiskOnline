79dc13d05ab6f5b15989479c1bd4089d08f0226d
jjokb8 <jjokb8@mail.missouri.edu>
2014-11-09 17:50:22 -0600

    Add controller

---
+++ api/controllers/AdjRegionsController.js
@@ -0,0 +1,11 @@
+/**
+ * AdjRegionsController
+ *
+ * @description :: Server-side logic for managing adjregions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
