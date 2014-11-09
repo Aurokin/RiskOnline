b12a4aa24fc4ca5950499c086b0b0aa7e15efe71
Summer <throwawaysummer@gmail.com>
2014-11-09 17:18:58 -0600

    updated and corrected some typos

--- assets/js/testing_log.js
+++ assets/js/testing_log.js
@@ -6,7 +6,7 @@ app.configure(function (){
 	app.use(express.cookieParser());
 	app.use(express.session({secret: 'secret', key: 'express.sid'}));
 	app.use(function (req, res){
-		res.end('<h2>Hello, your session id is ' + req.sessionID + '<h2>);
+		res.end('<h2>Hello, your session id is ' + req.sessionID + '<h2>');
 	});
 });
 
