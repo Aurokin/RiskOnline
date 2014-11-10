fa03f3e408f676030c8c194d78b40b1bb28df971
Hunter Sadler <hbs8vf@mail.missouri.edu>
2014-11-09 22:20:41 -0600

    Added relationship table between games and users

---
+++ api/controllers/GamesUsersLinkController.js
@@ -0,0 +1,11 @@
+/**
+ * GamesUsersLinkController
+ *
+ * @description :: Server-side logic for managing gamesuserslinks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
