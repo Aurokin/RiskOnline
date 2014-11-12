a541637a1389fe4e13e5e567312ec00f7ce0068c
Hunter Sadler <hbs8vf@mail.missouri.edu>
2014-11-11 23:00:20 -0600

    Add GameRegionLink

---
+++ api/models/GameRegionLink.js
@@ -0,0 +1,23 @@
+/**
+* GameRegionLink.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+  	gameID: {
+  		type: 'integer',
+  		required: true
+  	},
+
+  	regionID: {
+  		type: 'integer',
+  		required: true
+  	}
+  }
+};
+
