c3de7d76c0efd38fcf15f1902f135fbc81800325
Evan Gibler <evgibs@Evans-MacBook-Pro.local>
2014-11-13 13:31:34 -0600

    cleaned up code a litte:

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,39 +4,44 @@
  * @description :: Server-side logic for managing Users
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var bcrypt = require('bcrypt');
 
-module.exports = {
-	login: function (req, res) {
-
-		var bcrypt = require('bcrypt');
-
-		User.findOne({
-			email: req.body.email
-		}, function(err, user) {
-			if (err) {
-				res.json({ error: 'DB error' }, 500);
-			}
-
-			if (!user) {
-				res.view('static/index');
-			}
-			else {
-				bcrypt.compare(req.body.password, user.password, function(err, match) {
-					if (match) {
-						//console.log('login');
-						req.session.name = user.name;
-						req.session.user = user.id;
-						res.view('static/index');
-					}
-				});
-			}
-		});
-	},
-
-	logout: function (req, res) {
-		req.session.destroy();
-		res.view('static/index');
-	}
-
+module.exports =
+	{
+		login: function (req, res)
+			{
+				User.findOne
+					({
+						email: req.body.email
+					 },
+				function(err, user)
+					{
+						if (err)
+							{
+								res.json({ error: 'DB error' }, 500);
+							}
+						if (!user)
+							{
+								res.view('static/index');
+							}
+			 			else
+							{
+								bcrypt.compare(req.body.password, user.password, function(err, match)
+									{
+										if (match)
+											{
+												//console.log('login');
+												req.session.name = user.name;
+												req.session.user = user.id;
+												res.view('static/index');
+											}
+									});
+							}
+					});
+			},
+	logout: function (req, res)
+		{
+			req.session.destroy();
+			res.view('static/index');
+		}
 };
-
