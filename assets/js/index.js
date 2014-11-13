6b05a94435f2c1b3729d92fd7b0b468a0519ada8
Lindsey <lda7d7@mail.missouri.edu>
2014-11-12 17:35:50 -0800

    Deleted a break

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,11 +10,11 @@ server = http.createServer(function(req, res){
 		fs.readFile(__dirname + path, function(err, data){
 				if (err) return send404(res);
 				res.write('Stuff is working!\n');
-				res.writeHead(200, {'Content-Type' : path == 'json.js' ? 'text/javascript' : 'text/html'})
+				res.writeHead(200, {'Content-Type' : path == 'json.js' ? 'text/javascript' : 'text/html'});
 				res.write(data, 'utf8');
 				res.end();
 		});
-		break;
+		//break;
 	default: send404(res);
 		
 	}
