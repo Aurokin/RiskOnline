a8bbdf12b8172ea3956880a917a59cbe5ba99a23
jjokb8 <jjokb8@mail.missouri.edu>
2014-11-13 16:40:23 -0600

    Add gamelist

--- config/routes.js
+++ config/routes.js
@@ -42,7 +42,10 @@ module.exports.routes = {
   '/logout': 'UserController.logout',
   '/gameState': 'GamesController.gameState',
   'post /addUserToGame': 'GamesController.addPlayer',
-  'post /removeUserFromGame': 'GamesController.removePlayer'
+  'post /removeUserFromGame': 'GamesController.removePlayer',
+  '/gameList': {
+    view: 'gamelist'
+  }
 
   /***************************************************************************
   *                                                                          *
