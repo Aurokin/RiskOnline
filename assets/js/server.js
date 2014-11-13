cace213842784bab302c3509f8971a25ce12ae85
Summer <throwawaysummer@gmail.com>
2014-11-13 16:30:18 -0600

    made a little server to start some initial testing

---
+++ assets/js/server.js
@@ -0,0 +1,43 @@
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+var io = require('socket.io');
+
+var server = http.createServer(function(request, response){
+	console.log('Connection');
+	var path = url.parse(request.url).pathname;
+
+	switch(path){
+		case '/':
+	
+			response.writeHead(200, {'Content-type': 'text/html'});
+			response.write('hello world');
+			break;
+
+		case 'map.html':
+
+			fs.readFile(__fs.readFile(__dirname + path, function(error, data){
+				if (error){
+					response.writeHead(404);
+					response.write("opps this doesn't exist - 404");
+				}
+
+				else{
+					response.writeHead(200, {"Content-Type": "text/html"});
+					response.write(data, "utf8");
+				}
+		});
+		break;
+
+		default:
+			response.writeHead(404);
+			response.write("opps this doesn't exist - 404");
+			break;
+	}
+	response.end();
+});
+
+sever.listen(1337);
+
+var io.listen(server);
+	
