2d0e9697625ce13f0b998d0be6f3bf0cbe602329
Evan Gibler <evgibs@Evans-MacBook-Pro.local>
2014-11-13 13:37:07 -0600

    moved bcrypt, cleaned up code again

--- api/models/User.js
+++ api/models/User.js
@@ -5,52 +5,60 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-module.exports = {
+var bcrypt = require('bcrypt');
 
-  attributes: {
-  	name: {
-		type: 'string',
-		required: true
-	},
-	title: {
-		type: 'string'
-	},
-	email: {
-		type: 'string',
-		email: true,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: 'string'
-	},
-	online: {
-		type: 'boolean',
-		defaultsTo: false
-	},
-	admin: {
-		type: 'boolean',
-		defaultsTo: false
-	},
-	games: {
-		collection: 'games',
-		via: 'players'
-	}
-  },
+module.exports =
+  {
+    attributes:
+      {
+    	   name:
+          {
+    		      type: 'string',
+    		      required: true
+  	      },
+  	     title:
+          {
+  		        type: 'string'
+  	      },
+  	     email:
+          {
+        		type: 'string',
+        		email: true,
+        		required: true,
+        		unique: true
+  	      },
+  	     password:
+          {
+  		      type: 'string'
+  	      },
+  	     online:
+          {
+        		type: 'boolean',
+        		defaultsTo: false
+  	      },
+  	     admin:
+          {
+        		type: 'boolean',
+        		defaultsTo: false
+  	      },
+  	     games:
+          {
+        		collection: 'games',
+        		via: 'players'
+  	      }
+     },
 
-  beforeCreate: function (attrs, cb) {
-    var bcrypt = require('bcrypt');
-
-    bcrypt.genSalt(10, function(err, salt) {
-      if (err) return cb(err);
-
-      bcrypt.hash(attrs.password, salt, function(err, hash) {
-        if (err) return cb(err);
-
-        attrs.password = hash;
-        cb();
-      });
-    });
+  beforeCreate: function (attrs, cb)
+    {
+      bcrypt.genSalt(10, function(err, salt)
+        {
+          if (err) return cb(err);
+          bcrypt.hash(attrs.password, salt, function(err, hash)
+            {
+              if (err) return cb(err);
+              attrs.password = hash;
+              cb();
+            });
+        });
    }
 };
-
