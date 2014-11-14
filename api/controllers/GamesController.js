65ceaadaa1ba79472592bddc546f0b929cbbcf06
Hunter Sadler <hbs8vf@mail.missouri.edu>
2014-11-14 00:53:47 -0600

    Add One Game To Many Regions, Update GameState

--- api/controllers/GamesController.js
+++ api/controllers/GamesController.js
@@ -12,14 +12,29 @@ module.exports = {
 		var gameID = req.query.gameID;
 
 
-		Games.findOne({ id: gameID}).then(function(game){
+		Games.findOne({ id: gameID}).populate('regions').then(function(game){
 			var players = GamesUsersLink.find({
 				gameID: game.id
 			}).then(function(players) {
 				return players;
 			});
+
+			/*
+			http://stackoverflow.com/questions/23446484/sails-js-populate-nested-associations
+			var regions = Region.find({id: _.pluck(game.regions, 'region')
+			}).then(function(regions) {
+				return regions;
+			});
+			return [game, players, regions];
+			*/
+			
 			return [game, players];
-		}).spread(function (game, players) {
+		}).spread(function (game, players, regions) {
+			/*
+			var regions = _.indexBy(regions, 'id');
+			game.regions = _.map(game.region, function(region) {
+				region.region = regions[region.region];
+			});*/
 			return res.json({
 				game: game,
 				players: players
--- api/models/Region.js
+++ api/models/Region.js
@@ -21,6 +21,9 @@ module.exports = {
 		},
 		game: {
 			model: 'games'
+		},
+		region: {
+			model: 'regions'
 		}
 	}
 };
