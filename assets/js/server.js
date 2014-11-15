dd60e2d9f7818fac38d3cf1c2d6902e2f7846f10
Summer <throwawaysummer@gmail.com>
2014-11-15 14:15:04 -0600

    working on getting this fixed

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,24 +1,64 @@
 module.paths.push('/usr/local/lib/node_modules');
 
-var http = require("http");
-var url = require('url');
-var fs = require('fs');
-var pg = require('pg');
-
-var conString = "tcp://user:password@localhost:5432/db"; //
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var pg = require('pg-query');
+var conString = "postgres://postgres:Password@localhost:1337/postgres"; //
 var client = new pg.Client(conString);
+
 client.connect();
 
 var io = require('socket.io').listen(1337);
 
 io.sockets.on('connection', function (socket) {
     socket.on('sql', function (data) {
-        var query = client.query(data.sql, data.values);
-        query.on('row', function(row) {
+        var pg = client.query(data.sql, data.values);
+        pg.on('row', function(row) {
             socket.emit('sql', row);
         });
     });
 });
+
+
+/*Following logic is for my thinking purposes
+
+	io.socket.on('connection', function(socket){
+		socket.on('sql', function(data){
+				var pg = client.pg(data.sql, data.values);
+				pg.
+
+	})
+			send info to database to hold information on where resources are
+			send this updated information to client
+			check to see if it is the end of their turn
+			if(not){
+				allow them to distribute more resources
+				update database
+				check if they have made all moves available to them
+					if(not){
+							allow last distribution of resources
+							update database
+							update gamemap
+							end their turn and send information to other players the new game map
+
+				}
+			else{
+			end turn
+			update gamemap
+		}
+		update all players game map, lock current players id so they cannot play again
+		until others have played
+		update their game map
+		}
+});
+
+	on('disconnect', function(request, respond){
+
+});
+*/
 
 //Following lines are a simple server I was trying to make function;
 //however, something was conflicting in the module under config and
