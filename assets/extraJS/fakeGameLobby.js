bdc32864c071a2a62df280f98ce76572eba66d72
Hunter Sadler <hbs8vf@mail.missouri.edu>
2014-11-15 18:34:20 -0600

    Socket Connect Message on FakeGameList

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	console.log('hello');
 });
+
+io.socket.on('connect', function socketConnected() {
+
+	console.log("This is from the connect: ", this.socket.sessionid);
+
+});
