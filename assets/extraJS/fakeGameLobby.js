dc71ff6e87b1daa93cda9af3d7735ba39c8fda80
Hunter Sadler <hbs8vf@mail.missouri.edu>
2014-11-16 17:03:19 -0600

    Fix Sitewide Socket Call

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -1,6 +1,4 @@
 io.socket.on('connect', function socketConnected() {
-
-	console.log("This is from the connect: ", this.socket.sessionid);
 	
 	io.socket.get("/games", function(resData, jwres) {
 		console.log(resData);
--- assets/js/siteSocket.js
+++ assets/js/siteSocket.js
@@ -2,4 +2,4 @@ io.socket.on('connect', function socketConnected() {
 
 	console.log("This is from the connect: ", this.socket.sessionid);
 
-}
+});
