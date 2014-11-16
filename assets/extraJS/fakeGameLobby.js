8aed49964d0df5443d71228ff09942760d60c3e1
Summer <throwawaysummer@gmail.com>
2014-11-16 13:10:13 -0600

    adding some functionality, hopefully

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -1,3 +1,20 @@
+var pg = require("pg");
+var conString = "pg://admin:guest@localhost:1337/Database";
+
+var client = new pg.Client(conString);
+
+client.connect();
+
+var query = client.query("SELECT gameID FROM games");
+query.on("row", function(row, result){
+	result.addRow(row);
+});
+
+query.on("end", function(result){
+	console.log(JSON.stringify(result.rows, null, " "));
+	client.end();
+
+});
 
 $(document).ready(function() {
 	console.log('hello');
@@ -12,7 +29,9 @@ io.socket.on('connect', function socketConnected(){
 io.socket.post("/games", function(reData, jwres){
 
 		console.log("This is from socketpost: ", reData);
-		for(var i = 0; i < reData; i++){
-			result.push(reData);
-		}
+		var query = client.query("SELECT gameID FROM games");
+		query.on("row", function(row, result){
+			result.addRow(row);
+		});
+
 });
