41a602d82e2f4bb02ed2ebc89b2288f272d4c488
Summer <throwawaysummer@gmail.com>
2014-11-15 19:17:36 -0600

    more error fixing

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -6,20 +6,8 @@ io.socket.on('connect', function socketConnected() {
 
 	console.log("This is from the connect: ", this.socket.sessionid);
 
-	socket.get(‘/user/welcome’, function gotResponse () {
-    // we don’t really care about the response
-  });
-
 });
 
-io.socket.on('connect', function socketConnected(){
-
+io.socket.on('connection', function(socket){
+		socket.emit('user connected');
 });
-
- welcome: function (req, res) {
-    // Get all of the users
-    region.find().exec(function (err, region) {
-      // Subscribe the requesting socket (e.g. req.socket) to all users (e.g. users)
-      region.subscribe(req.socket, region);
-    });
-  }
