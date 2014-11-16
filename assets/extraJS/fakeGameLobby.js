36586229c35dde2f2367f7b638fda3d68fefec1c
Summer <throwawaysummer@gmail.com>
2014-11-15 19:11:25 -0600

    checking out something to see if it works

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -6,11 +6,20 @@ io.socket.on('connect', function socketConnected() {
 
 	console.log("This is from the connect: ", this.socket.sessionid);
 
+	socket.get(‘/user/welcome’, function gotResponse () {
+    // we don’t really care about the response
+  });
+
 });
 
-io.socket.on('connection', function socketConnected(){
-		socket.on('hi', function(){
-			data = { name: 'Test', message: 'Hi Too'};
-			io.socket.emit('init', data);
-		});
+io.socket.on('connect', function socketConnected(){
+
 });
+
+ welcome: function (req, res) {
+    // Get all of the users
+    region.find().exec(function (err, region) {
+      // Subscribe the requesting socket (e.g. req.socket) to all users (e.g. users)
+      region.subscribe(req.socket, region);
+    });
+  }
