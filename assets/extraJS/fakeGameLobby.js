5ae25f8e8af30c19396a4250541efed7667e6eb4
Summer <throwawaysummer@gmail.com>
2014-11-15 19:03:11 -0600

    testing a few lines myself

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -7,3 +7,10 @@ io.socket.on('connect', function socketConnected() {
 	console.log("This is from the connect: ", this.socket.sessionid);
 
 });
+
+io.socket.on('connection', function(socket){
+		socket.on('hi', function(){
+			data = { name: 'Test', message: 'Hi Too'};
+			io.socket.emit('init', data);
+		});
+});
