cca462dd71c0adc0e6ba906a4f815ac68f3c0eeb
Summer <throwawaysummer@gmail.com>
2014-11-15 20:23:10 -0600

    trying to work on getting it to work

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -8,6 +8,17 @@ io.socket.on('connect', function socketConnected() {
 
 });
 
-io.socket.get('/database.sql', function(resdata){
-	resdata;
-});
+io.socket.on('connect', function(socket){
+		socket.emit('greeting', 'Hello');
+		setInterval(5000, function(data){
+			var uid = data['uid'];
+			var q = "SELECT * FROM users";
+			connection.query(q, function(err, rows, fields){
+				if(err) throw err;
+				if(row[0].userID = userID){
+					socket.emit('new_message', rows[0]);
+					userID = rows[0].id
+				}
+			});
+		});
+	});
