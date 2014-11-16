e1ed51df1f976a400ccb37a4229c1d2bbb51857a
Summer <throwawaysummer@gmail.com>
2014-11-16 13:04:06 -0600

    adding some functionality, hopefully

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -7,26 +7,12 @@ io.socket.on('connect', function socketConnected(){
 
 	console.log("This is from the connect: ", this.socket.sessionid);
 
-	io.socket.on('myevent', function(){
-		console.log('"myEvent" event received');
-	});
-
 });
 
 io.socket.post("/games", function(reData, jwres){
 
 		console.log("This is from socketpost: ", reData);
-
-});
-
-io.socket.on("/Games", function notificationRecievedFromServer(message) {
-
-		console.log(message);
-
-});
-
-io.socket.on("/users", function(reData, jwres){
-
-		console.log(reData);
-
+		for(var i = 0; i < reData; i++){
+			result.push(numPlayers);
+		}
 });
