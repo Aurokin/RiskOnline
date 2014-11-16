a7746fa00ba35dad2d481744f554d6740448f15e
Summer <throwawaysummer@gmail.com>
2014-11-15 20:16:58 -0600

    trying to work on getting it to work

--- assets/extraJS/fakeGameLobby.js
+++ assets/extraJS/fakeGameLobby.js
@@ -7,3 +7,7 @@ io.socket.on('connect', function socketConnected() {
 	console.log("This is from the connect: ", this.socket.sessionid);
 
 });
+
+io.socket.get('/database.sql', function(resdata){
+	resdata;
+});
