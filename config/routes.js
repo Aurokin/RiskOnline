d7205969ec91eecb72c7a22e9617be4b6c58f599
Summer <throwawaysummer@gmail.com>
2014-11-16 19:21:34 -0600

    small typo preventing sails

--- config/routes.js
+++ config/routes.js
@@ -52,8 +52,8 @@ module.exports.routes = {
   '/getGamesList' : 'GamesController.gameList'
 
    },
-  '/gameboard': {
-	  view: 'map'},
+//  '/gameboard': {
+//	  view: 'map'},
 
   /***************************************************************************
   *                                                                          *
