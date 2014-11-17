ea4f342d5a8db0d51eb5ec347c34eef582b5da62
Summer <throwawaysummer@gmail.com>
2014-11-16 19:19:08 -0600

    initial creation of new workspace

---
+++ assets/js/sPractice/serverlogic.js
@@ -0,0 +1,18 @@
+io.socket.on('connect', function socketConnected(){
+
+  console.log("This is from the connect: ", this.socket.sessionid);
+
+});
+
+io.socket.get("/games", function(reData, jwres){
+
+    console.log(reData);
+
+});
+
+getRoomsList: function(req, res) {
+    var roomNames = JSON.stringify(sails.sockets.rooms());
+    res.json({
+      message: 'A list of all the rooms: '+roomNames
+    });
+}
