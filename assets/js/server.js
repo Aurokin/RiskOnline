699e2867641257acb7e6b3971b33a7bac7d71c18
Summer <throwawaysummer@gmail.com>
2014-11-30 12:41:33 -0600

    added a FK for gameID to database

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,1 +1,6 @@
-
+$("go").click(function(){
+  socket.post("/game/addTerritory", userID, troops, regionID);
+}, function(res, info{
+  $("region.id.owner").find("army count").text(resp.value)
+});
+});
