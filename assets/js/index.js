a4ebcbd499c790cb0885fda0a63ff1401b532944
Lindsey <lda7d7@mail.missouri.edu>
2014-11-30 20:02:40 -0800

    Attempted to change path so code would execute

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,26 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-app.get('/', function(req, res){
-        res.sendFile(__dirname + '/real_time.ejs');
+app.all('*', function(req, res){
+	res.render('real_time.ejs');	
+//res.sendFile(__dirname + '/../../views/real_time.ejs');
+});
 
 io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-
       	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
+		io.emit('chat message', msg);
 	});
-
-          socket.on('chat message', function(msg){
-                io.emit('chat message', msg);
-                });
-});
 });
 
 http.listen(1337, function(){
-        console.log('lisening on *:1337');
+        console.log('listening on *:1337');
 });
