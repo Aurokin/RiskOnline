68e1a6e0606d480d8ddee5fb3a0d1a701b37ddb1
Hunter Sadler <hsadlersemail@gmail.com>
2014-12-03 22:58:45 -0600

    Detect Variables For Game Create

--- assets/js/gameCreate.js
+++ assets/js/gameCreate.js
@@ -1,4 +1,8 @@
 $('#create-game').submit(function(e) {
   e.preventDefault();
+  var gameName = $('#gameName').val();
+  var password = $('#password').val();
+  var numPlayers = parseInt($('#numPlayers').val());
+  console.log(gameName+' '+password+' '+numPlayers);
   console.log('Submitted Form');
 });
